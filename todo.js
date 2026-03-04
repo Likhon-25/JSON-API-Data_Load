809fc7b358dd44b4ba851f3c948c6ba4d4324dd5
Likhon-25 <likhonmondol327@gmail.com>
2026-03-05 00:53:01 +0600

    todo list compleate

--- todo.js
+++ todo.js
@@ -1,18 +1,31 @@
-const loadTodo = () =>{
-    const url ="https://jsonplaceholder.typicode.com/todos";
+const loadTodo = () => {
+    const url = "https://jsonplaceholder.typicode.com/todos";
 
     fetch(url)
-    .then((res) => res.json())
-    .then((json) => displayTodo(json));
+        .then((res) => res.json())
+        .then((json) => displayTodo(json));
 }
 
-const displayTodo = (todos) =>{
- const todoContainer = document.getElementById("todo-container");
- todoContainer.innerHTML ="";
+const displayTodo = (todos) => {
+    const todoContainer = document.getElementById("todo-container");
+    todoContainer.innerHTML = "";
 
- todos.forEach((todo) =>{
-    console.log(todo);
- } )
- 
+    todos.forEach((todo) => {
+        console.log(todo);
+
+        const todoCard = document.createElement("div");
+        todoCard.innerHTML = `
+    <div class = "todo-card">
+    <p>${todo.completed == true
+                ? `<i class="fa-regular fa-square-check"></i>`
+                : `<i class="fa-solid fa-square-check"></i>`}
+        ${todo.completed}</p>
+    <h4>${todo.title}</h4>
+    </div>
+    `
+
+        todoContainer.append(todoCard)
+    })
+
 }
 loadTodo()
